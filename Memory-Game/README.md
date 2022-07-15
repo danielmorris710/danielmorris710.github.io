b2e1295a92b92663ea8eba8bded44ddf4893d54f
Daniel Morris <uptownmelo#gmail.com>
2022-07-15 18:49:39 -0400

    first commit

---
+++ Memory-Game/README.md
@@ -0,0 +1,45 @@
+# NBA Memory Game 
+NBA Memory Game is your simple memory game but with a twist. You can either flip cards by random or use the details on the front of the cards. Player with the most matches wins.
+
+<br>
+<br>
+
+## Preview
+![Begin Game](images/Screenshot1.jpg)
+<br>
+<br>
+
+## Game Play
+Pick cards based off of pure luck or use details on the front.
+Go back and forth until all cards have been matched up.
+
+<br>
+<br>
+
+## User Stories
+- Choose wether you are Home Team or Away Team
+- Home Team always goes first
+- Flip cards off of luck or details
+- Learn some interesting facts about past and current NBA players 
+
+<br>
+<br>
+
+## Technologies Used 
+- HTML 5
+- CSS 3
+- JavaScript
+
+<br>
+<br>
+
+## Future Plans For Rock Paper Scissors 
+During this project, I realized there was so much more I wanted to do. 
+<br>
+<br>
+A few things I would do to improve the players experience :
+- Being able to select the first cards the previous player chose 
+- Add a more players  
+- A restart button to not have to refresh the page 
+- Solo play mode against the computer
+- Different levels
---
+++ Memory-Game/memory.js
@@ -0,0 +1,109 @@
+let cards = document.querySelectorAll('.memory-card')
+
+let flippedCard = false
+let gameTable = false
+let firstCard, secondCard
+let counter = 1 
+let playerOneScore = 0
+let playerTwoScore = 0
+let playerOne = document.getElementById('player-score')
+let playerTwo = document.getElementById('player-two-score')
+playerOne.innerHTML = playerOneScore
+playerTwo.innerHTML = playerTwoScore
+let whosTurn = document.getElementById('turns')
+ 
+whosTurn.innerHTML = "Home Team's Turn "
+
+
+// flip card function
+function flipCards() {
+  if (gameTable) return
+  if (this === firstCard) return
+
+  this.classList.toggle('flip')
+
+  if (!flippedCard) {
+    flippedCard = true
+    firstCard = this
+
+    return
+  }
+  
+  flippedCard = false
+  secondCard = this
+  checkCardMatch()
+}
+
+// check if the cards match if not flip them back 
+function checkCardMatch() {
+  
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+ if(!isMatch){
+    counter++
+  } 
+
+  isMatch ? disableCards() : unflipCards()
+
+  if(counter % 2 === 1 && isMatch){
+    playerOneScore++
+    playerOne.innerHTML = playerOneScore
+  }else if (counter % 2 === 0 && isMatch){
+    playerTwoScore++ 
+    playerTwo.innerHTML = playerTwoScore
+  }
+
+}
+
+// dont let them flip back if they match
+function disableCards() {
+  firstCard.removeEventListener('click', flipCards)
+  secondCard.removeEventListener('click', flipCards)
+
+  resetCards()
+}
+
+// flip cards back if they don't match
+function unflipCards() {
+  gameTable = true
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip')
+    secondCard.classList.remove('flip')
+
+    resetCards()
+  }, 1500)
+
+  if(counter % 2 === 1 ){
+    whosTurn.innerHTML = "Home Team's Turn "
+
+  }else if (counter % 2 === 0){
+    whosTurn.innerHTML = "Away Team's Turn "
+  }
+}
+
+// random shuffling cards
+(function shuffle() {
+  cards.forEach(card => {
+    let arrangeCards = Math.floor(Math.random() * 20)
+    card.style.order = arrangeCards
+  })
+})()
+
+// able to flip cards again after non match
+function resetCards() {
+  [flippedCard, gameTable] = [false, false]
+  [firstCard, secondCard] = [null, null]
+}
+
+cards.forEach(card => card.addEventListener('click', flipCards))
+
+// make two players take turns 
+function takeTurns(){
+  let counter = 0 
+  let playerOneScore = 0
+  let playerTwoScore = 0
+  if(counter % 2 === 1){
+
+  }
+}
+
